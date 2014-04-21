6fdd679e3653e96c24219da1d2a1a9ba0341d816
aryner <aryner@gmail.com>
2014-04-21 10:45:42 -0700

    prob2 completely done

--- homework3/prob2.py
+++ homework3/prob2.py
@@ -14,14 +14,14 @@ def sinC(x):
   else:
     return (np.sin(2*np.pi*x))/(2*np.pi*x)
 
-#numPoints = 5050000
-#x = generateX(numPoints, 0, 0.5)
-#y = [sinPi(x[i]) for i in range(0,numPoints)]
+numPoints = 5050000
+x = generateX(numPoints, 0, 0.5)
+y = [sinPi(x[i]) for i in range(0,numPoints)]
 
 one = 1/np.pi
-#print 1/np.pi - trapezoidal(x,y)
-#print 1/np.pi - romberg(0, 0.5, 1675000, sinPi)
-#print 1/np.pi - gaussQuadrature(0, 0.5, sinPi, 4)
+print 1/np.pi - trapezoidal(x,y)
+print 1/np.pi - romberg(0, 0.5, 1675000, sinPi)
+print 1/np.pi - gaussQuadrature(0, 0.5, sinPi, 4)
 print ""
 plt.figure(1)
 plt.title('a')
@@ -30,13 +30,13 @@ yp1 = [sinPi(xp1[i]) for i in range(0,100)]
 plt.plot(xp1,yp1,'b')
 
 numPoints2 = 1500000
-#x2 = generateX(numPoints2, 1.,2.)
-#y2 = [eToSomeCrap(x2[i]) for i in range(0, numPoints2)]
+x2 = generateX(numPoints2, 1.,2.)
+y2 = [eToSomeCrap(x2[i]) for i in range(0, numPoints2)]
 
 two = 0.3406636214304
-#print two - trapezoidal(x2,y2)
-#print two - romberg(1.,2., 500000, eToSomeCrap)
-#print two - gaussQuadrature(1.,2.,eToSomeCrap, 4)
+print two - trapezoidal(x2,y2)
+print two - romberg(1.,2., 500000, eToSomeCrap)
+print two - gaussQuadrature(1.,2.,eToSomeCrap, 4)
 print ""
 plt.figure(2)
 plt.title('b')
@@ -45,13 +45,13 @@ yp2 = [eToSomeCrap(xp2[i]) for i in range(0,100)]
 plt.plot(xp2,yp2,'b')
 
 numPoints3 = 7000
-#x3 = generateX(numPoints3, -1.,1.)
-#y3 = [sinC(x3[i]) for i in range(0, numPoints3)]
+x3 = generateX(numPoints3, -1.,1.)
+y3 = [sinC(x3[i]) for i in range(0, numPoints3)]
 
 three = 0.45141166679014
-#print three - trapezoidal(x3,y3)
-#print three - romberg(-1.,1., 310, sinC)
-#print three - gaussQuadrature(-1.,1., sinC, 9)
+print three - trapezoidal(x3,y3)
+print three - romberg(-1.,1., 310, sinC)
+print three - gaussQuadrature(-1.,1., sinC, 9)
 plt.figure(3)
 plt.title('c')
 xp3 = generateX(100,-1.,1.)
